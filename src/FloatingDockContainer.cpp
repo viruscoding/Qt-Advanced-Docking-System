04f6d9168e159f07de565c5159ecd4ea16ab5be1
Uwe Kindler <uwe_kindler@web.de>
2024-07-08 14:32:33 +0200

    Fixed #642 - The floating window can not back to normal size after maximizing it

--- src/FloatingDockContainer.cpp
+++ src/FloatingDockContainer.cpp
@@ -1331,7 +1331,8 @@ void CFloatingDockContainer::onMaximizeRequest()
 //============================================================================
 void CFloatingDockContainer::showNormal(bool fixGeometry)
 {
-	if (windowState() == Qt::WindowMaximized)
+    if ( (windowState() & Qt::WindowMaximized) != 0 ||
+         (windowState() & Qt::WindowFullScreen) != 0)
 	{
 		QRect oldNormal = normalGeometry();
 		Super::showNormal();

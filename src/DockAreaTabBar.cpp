1f21eb0200adf1a480913add64cd56f4ecdec6e0
Ivan Ugryumov <smille2003@yandex.ru>
2024-12-12 15:13:01 +0300

    Improved tab bar scrolling (#678)
    * Improved tab bar scrolling
    
    * Fix: tab bar scroll for mouse

--- src/DockAreaTabBar.cpp
+++ src/DockAreaTabBar.cpp
@@ -160,18 +160,8 @@ CDockAreaTabBar::~CDockAreaTabBar()
 //============================================================================
 void CDockAreaTabBar::wheelEvent(QWheelEvent* Event)
 {
-	Event->accept();
-	const int direction = Event->angleDelta().y();
-	if (direction < 0)
-	{
-		horizontalScrollBar()->setValue(horizontalScrollBar()->value() + 20);
-	}
-	else
-	{
-		horizontalScrollBar()->setValue(horizontalScrollBar()->value() - 20);
-	}
+    QCoreApplication::sendEvent(horizontalScrollBar(), Event);
 }
-
 
 //============================================================================
 void CDockAreaTabBar::setCurrentIndex(int index)
@@ -356,7 +346,7 @@ void CDockAreaTabBar::onCloseOtherTabsRequested()
 				CDockWidget::DockWidgetDeleteOnClose) ? 1 : 0;
 			closeTab(i);
 
-			// If the the dock widget blocks closing, i.e. if the flag
+			// If the dock widget blocks closing, i.e. if the flag
 			// CustomCloseHandling is set, and the dock widget is still open,
 			// then we do not need to correct the index
 			if (Tab->dockWidget()->isClosed())
